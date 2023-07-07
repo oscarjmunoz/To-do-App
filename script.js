61d515118ab46f3d0ff16a54019a570dd73d72fe
oscar <oscarmunoz03@gmail.com>
2023-07-06 21:28:31 -0500

    add components

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-( () =>{ 
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+
 const btn = document.querySelector("[data-form-btn]")
 
 const createtask = (evento) => {
@@ -12,36 +14,23 @@ const createtask = (evento) => {
     //backstiks
     
     const taskContent = document.createElement("div")
-    taskContent.appendChild(checkComplete());
+
     const titleTask = document.createElement("span")
     titleTask.classList.add("task");
     titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
-
-    const content = `<i class="fas fa-trash-alt trashIcon icon"></i>`;
-    //task.innerHTML = content ; 
+    
+        //task.innerHTML = content ; 
     task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
     list.appendChild(task);
-        
+    
 };
 
 //arrow function-funcion flecha o funciones anonimas
 btn.addEventListener("click", createtask)
 
-const checkComplete = () =>{
-    const i = document.createElement("i")
-    i.classList.add("far","fa-check-square","icon");
-    i.addEventListener("click", completeTask);
 
-    return i;
-};
-//Immediately Invoked fucntion expression - IIFE
 
-const completeTask = (event) => {
-    const element = event.target;
-    element.classList.toggle("fas");
-    element.classList.toggle("completeIcon");
-    element.classList.toggle("far");
-};
-}
-)();
+
